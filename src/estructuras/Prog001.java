a5fd3d5c475e0a9d9ff10c846f0000a305d01a3a
David Esteban <dm7659746@gmail.com>
2020-06-30 15:25:37 -0500

    Prog001 listo

---
+++ src/estructuras/Prog001.java
@@ -0,0 +1,18 @@
+
+package estructuras;
+
+import java.util.Scanner;
+
+public class Prog001 {
+    public static void main(String[] args){
+    Scanner teclado = new Scanner(System.in);
+    int numero;
+    System.out.print("numero: ");
+    numero = Integer.valueOf(teclado.nextLine());
+    if(numero%2==0){
+        System.out.println("es par");
+    }else{
+        System.out.println("es impar");
+    }
+}
+}
